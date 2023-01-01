86842cdfafe8e464c5f42cdd65553779b1af7983
OAN <oan@fedora.home>
2023-01-01 18:12:07 +0100

    feat : add class and function for computing. bug : the operator don't display after clicks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,36 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement 
+        this.clear()
+    }
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+    delete() {
+        
+    }
+    appendNumber(number) { // when user select number
+        if( number === '.' && this.currentOperand.includes('.')) return 
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+    chooseOperation(operation) { // when user select operation
+        if(this.currentOperand === '') return
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+    compute() {
+
+    }
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand // first display in current display
+        this.previousOperandTextElement.innerText = this.previousOperand // When user select operrator => display above on previous display
+    }
+}
+
 const numberButtons= document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
@@ -5,3 +38,21 @@ const deleteButtons = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement= document.querySelector('[data-previous-operand]');
 const currentOperandTextElement= document.querySelector('[data-current-operand]');
+
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
